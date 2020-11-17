7428786bc9e68e1a8b113e2586fb11f2c2a2cbcf
CH.Hannan <ch.hannan15@gmail.com>
2020-11-17 19:18:06 +0500

    Added packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -16,9 +20,14 @@ import {AppComponent} from './app.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
     DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
