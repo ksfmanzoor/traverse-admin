6b1eb6411ab6527312835538ae7b2cc66b861f31
CH.Hannan <ch.hannan15@gmail.com>
2020-12-07 16:02:57 +0500

    Added departure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {FormControl} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 import {MatSelect} from '@angular/material/select';
 import {ReplaySubject} from 'rxjs';
+import {Departure, Package} from 'src/app/models/trip';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +10,19 @@ import {ReplaySubject} from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  addTripForm: FormGroup;
   title = 'traverse-admin';
   panelOpenState = false;
-  toppings = new FormControl();
-  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
-  foods = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
+  groupList: string[] = ['pre', 'common', 'post'];
+  availableLocations = [
+    {value: 'Islamabad', viewValue: 'Islamabad'},
+    {value: 'Lahore', viewValue: 'Lahore'},
+    {value: 'Karachi', viewValue: 'Karachi'}
   ];
+
+  packagesList: Package[] = [];
+  departuresList: Departure[] = [];
+  isEditMode = false;
 
   availableAttractions = [
     'Get to work',
@@ -34,7 +39,8 @@ export class AppComponent implements OnInit {
     'Walk dog'
   ];
 
-  protected banks = [{name: 'Bank A (Switzerland)', id: 'A'},
+  allAttractions = [
+    {name: 'Bank A (Switzerland)', id: 'A'},
     {name: 'Bank B (Switzerland)', id: 'B'},
     {name: 'Bank C (France)', id: 'C'},
     {name: 'Bank D (France)', id: 'D'},
@@ -53,21 +59,40 @@ export class AppComponent implements OnInit {
     {name: 'Bank Q (Germany)', id: 'Q'},
     {name: 'Bank R (Germany)', id: 'R'}];
 
-  /** control for the selected bank for multi-selection */
-  public bankMultiCtrl: FormControl = new FormControl();
-
-  /** control for the MatSelect filter keyword multi-selection */
-  public bankMultiFilterCtrl: FormControl = new FormControl();
-
-  /** list of banks filtered by search keyword */
-  public filteredBanksMulti: ReplaySubject<any[]> = new ReplaySubject<any[]>(1);
+  bankMultiFilterCtrl: FormControl = new FormControl();
+  filteredBanksMulti: ReplaySubject<any[]> = new ReplaySubject<any[]>(1);
 
   @ViewChild('multiSelect', {static: true}) multiSelect: MatSelect;
 
 
   ngOnInit() {
+    this.addTripForm = new FormGroup({
+        tripTitle: new FormControl(''),
+        slug: new FormControl(''),
+        overview: new FormControl(''),
+        attractions: new FormControl(),
+        packageTitle: new FormControl(''),
+        packagePrice: new FormControl(''),
+        packageStandard: new FormControl(false),
+        departureLocation: new FormControl(''),
+        departureVia: new FormControl(''),
+        departurePrice: new FormControl(''),
+        departureDate: new FormControl(''),
+        departureTime: new FormControl(''),
+        arrivalDate: new FormControl(''),
+        arrivalTime: new FormControl(''),
+        departureStandard: new FormControl(false),
+        itineraryDate: new FormControl(''),
+        itineraryGroup: new FormControl(''),
+        itineraryDepartures: new FormControl(''),
+        itineraryBody: new FormControl(''),
+        itineraryServiceType: new FormControl(''),
+        itineraryServiceValue: new FormControl(''),
+        itineraryPackages: new FormControl(''),
+      }
+    );
     // load the initial bank list
-    this.filteredBanksMulti.next(this.banks.slice());
+    this.filteredBanksMulti.next(this.allAttractions.slice());
 
     // listen for search field value changes
     this.bankMultiFilterCtrl.valueChanges
@@ -77,21 +102,72 @@ export class AppComponent implements OnInit {
       });
   }
 
+  get formControl() {
+    return this.addTripForm.controls;
+  }
 
-  protected filterBanksMulti() {
-    if (!this.banks) {
+  filterBanksMulti() {
+    if (!this.allAttractions) {
       return;
     }
     let search = this.bankMultiFilterCtrl.value;
     if (!search) {
-      this.filteredBanksMulti.next(this.banks.slice());
+      this.filteredBanksMulti.next(this.allAttractions.slice());
       return;
     } else {
       search = search.toLowerCase();
     }
     this.filteredBanksMulti.next(
-      this.banks.filter(bank => bank.name.toLowerCase().indexOf(search) > -1)
+      this.allAttractions.filter(bank => bank.name.toLowerCase().indexOf(search) > -1)
     );
+  }
+
+  addPackage() {
+    this.packagesList.push({
+      title: this.formControl.packageTitle.value,
+      price_per_person: this.formControl.packagePrice.value,
+      is_standard: this.formControl.packageStandard.value
+    });
+    this.formControl.packageTitle.setValue('');
+    this.formControl.packagePrice.setValue('');
+    this.formControl.packageStandard.setValue(false);
+
+  }
+
+  updatePackage(packageInfo: Package) {
+    this.formControl.packageTitle.setValue(packageInfo.title);
+    this.formControl.packagePrice.setValue(packageInfo.price_per_person);
+    this.formControl.packageStandard.setValue(packageInfo.is_standard);
+  }
+
+  deletePackage(title) {
+    const index = this.packagesList.findIndex(obj => obj.title === title);
+    if (index > -1) {
+      this.packagesList.splice(index, 1);
+    }
+  }
+
+  addDeparture() {
+    this.departuresList.push({
+      location: this.formControl.departureLocation.value,
+      via: this.formControl.departureVia.value,
+      price_per_person: this.formControl.departurePrice.value,
+      departure_date: this.formControl.departureDate.value,
+      arrival_date: this.formControl.arrivalDate.value,
+      is_standard: this.formControl.departureStandard.value
+    });
+    this.formControl.departureLocation.setValue('');
+    this.formControl.departureVia.setValue('');
+    this.formControl.departurePrice.setValue('');
+    this.formControl.departureDate.setValue('');
+    this.formControl.arrivalDate.setValue('');
+    this.formControl.departureStandard.setValue(false);
+  }
+
+  addTrip() {
+    console.log(this.addTripForm.value);
+    console.log(this.packagesList);
+    console.log(this.departuresList);
   }
 
 }
---
+++ src/app/models/trip.ts
@@ -0,0 +1,77 @@
+export interface Trip {
+  id: string;
+  title: string;
+  slug: string;
+  itinerary_days: ItineraryDay[];
+  overview: string;
+  packages: Package[];
+  departures: Departure[];
+  gallery_images: GalleryImage[];
+  attractions: Attraction[];
+}
+
+export interface TripService {
+  id: string;
+  title: string;
+  description: string;
+  image?: any;
+}
+
+export interface TripServiceValue {
+  id: string;
+  value: string;
+  trip_service: TripService;
+  packages: string[];
+}
+
+export interface ItineraryDay {
+  id: string;
+  date: string;
+  body: string;
+  departures: string[];
+  trip_service_values: TripServiceValue[];
+  group: string;
+}
+
+export interface Package {
+  title: string;
+  price_per_person: number;
+  is_standard: boolean;
+}
+
+export interface TripAddon {
+  id: string;
+  title: string;
+  description: string;
+}
+
+export interface TripAddonValue {
+  id: string;
+  price: number;
+  trip_addon: TripAddon;
+  package: string;
+}
+
+export interface Departure {
+  location: string;
+  via: string;
+  price_per_person: number;
+  departure_date: Date;
+  arrival_date: Date;
+  // trip_addon_values: TripAddonValue[];
+  is_standard: boolean;
+}
+
+export interface GalleryImage {
+  id: string;
+  image: string;
+  alt_text: string;
+}
+
+export interface Attraction {
+  id: string;
+  title: string;
+  subtitle: string;
+  thumbnail: string;
+  slug: string;
+}
